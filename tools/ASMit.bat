59454faba2b63cfc4c1ff640b79231423ced715b
lc@kun'sASUS <xsro@foxmail.com>
2020-07-10 21:54:36 +0800

    实现在linux和windows上的主要功能 Squashed commit of the env-ubuntu

---
+++ tools/ASMit.bat
@@ -0,0 +1,82 @@
+@echo off
+:: GB 2312
+if "%~f1" == "" goto info
+:SetValues
+set "cdo=%CD%"
+set "file=%~f1"
+set "mode=C"
+set "tool=%~dp0"
+set Print=0
+    set "test=%tool%work\"
+    set "test=%4"
+    if not exist %test% mkdir %test%
+    set "dosbox=%tool%dosbox\DOSBox.exe"
+    set mcd=-noautoexec ^
+    -c "mount c \"%tool%\"" -c "mount d \"%test%\"" ^
+    -c "d:"
+        ::change value according to input
+        if "%2" neq "" set "mode=%2"
+        if "%3" neq "" set "tool=%~f3"
+    if "%mode%" == "A" set Print=1
+    if "%mode%" == "B" set Print=1
+    if "%mode%" == "C" set Print=2
+    if "%mode%" == "D" set Print=2
+    ::improve the display resolution of dosbox
+    if "%Print%" == "0"set "bigboxconf=%tool%dosbox\bigbox.conf" 
+::if "%Print%" == "2" goto preDo
+:OutputInfo
+    echo Time:%time%
+    echo Mode:%mode% ASMtoolsfrom:%tool%
+    echo ASMfilefrom:%file%
+:preDo
+cd %test%
+    :ModeSelect
+        if %mode%==4 goto core
+        if %mode%==8 goto core
+    if "%~x1"==".ASM" goto NEXT
+    if "%~x1"==".asm" goto NEXT
+    echo %~x1 is not a supported assembly file
+    goto end
+    :NEXT
+        if not exist "%file%" echo no such file && goto end
+        if exist T.ASM del T.ASM
+        copy "%file%" T.ASM>nul
+        :core
+        start/min/wait %dosbox% %mcd% -c "asm/m %mode%"
+            if "%Print%" == "1" goto print1
+            if "%Print%" == "2" goto print2
+            goto end
+            :print1
+            echo [ASM information] OUTPUT:
+            type T.txt
+            if not exist T.OUT goto end
+            echo [YOUR program] OUTPUT:
+            type T.out
+            goto end
+            :print2
+            FOR /F "eol=; skip=3 tokens=* delims=, " %%i in (T.txt) do echo %%i
+            if not exist T.OUT goto end
+            echo [YOUR program] OUTPUT:
+            FOR /F "eol=; tokens=* delims=, " %%i in (T.out) do echo   %%i
+            goto end
+:info
+    echo "asmit.bat <file> [<mode>]<toolsdir> <workdir>]"
+    echo "<file> file to be used "
+    echo "<display> s"
+    echo "<mode> choose mode the way display default is 1"
+    echo " 0 copy the files open dosbox add path"
+    echo " 1 tasm run output in dosbox"
+    echo " 2 tasm run pause exit"
+    echo " 3 tasm run and td"
+    echo " 4 open turbo debugger at test folder"
+    echo " 5 masm run output in dosbox"
+    echo " 6 masm run pause exit"
+    echo " 7 masm and debug"
+    echo " 8 open masm debug at test folder"
+    echo " A tasm run output in shell"
+    echo " B masm run ouput in shell"
+    echo " C simplipfy output open turbo debugger at test folder"
+    echo " D simplipfy output open masm debug at test folder"
+    echo "<toolsdir> the tools folder with subdir masm,tasm,test"
+:end
+cd %cdo%
---
+++ tools/asmit.sh
@@ -0,0 +1,135 @@
+# !/bin/bash
+# this is a script for use dosbox to run TASM/MASM tools
+# set value
+tool="$(dirname $0)/"
+test="${tool}/work"
+bigboxconf="-conf \"$tool/dosbox/bigbox.conf\""
+mode='C'
+print=0
+# check whether installed dosbox
+if type dosbox
+    then 
+        echo !Msg:  dosbox installed
+    else
+        echo !Msg:  You need install dosbox first
+        echo !Msg:  *Maybe You can use:sudo apt install dosbox
+        exit
+    fi
+# check the file path
+if [ -z "$1" ]
+then
+    echo "./asmit.sh <file> [-d <toolsdir> -m <mode> -w <workdir>]"
+    echo "./asmit.sh -h for help"
+    exit
+# display the help
+elif [ "$1" = "-h" ]
+then
+    echo './asmit.sh <file> [-d <toolsdir> -m <mode> -w <workdir>]'
+    echo '<file> file to be used '
+    echo '<mode> choose mode the way display default is 1'
+    echo " 0 copy the files open dosbox add path"
+    echo " 1 tasm run output in dosbox"
+    echo " 2 tasm run pause exit"
+    echo " 3 tasm run and td"
+    echo " 4 open turbo debugger at test folder"
+    echo " 5 masm run output in dosbox"
+    echo " 6 masm run pause exit"
+    echo " 7 masm and debug"
+    echo " 8 open masm debug at test folder"
+    echo " A tasm run output in shell"
+    echo " B masm run ouput in shell"
+    echo " C simplipfy output open turbo debugger at test folder"
+    echo " D simplipfy output open masm debug at test folder"
+    echo '<tooldir> the tools folder with subdir masm,tasm'
+    echo '<workdir> the folder of asm.bat'
+# make sure the file readable and is a ASM file
+elif [ -r "$1" ]
+then
+    file=$1
+    # get the options
+    echo 
+    shift
+    while getopts :m:d: opt
+    do
+        case "$opt" in
+            m) mode=$OPTARG;;
+            d) tool=$OPTARG
+             if [ -d ${tool} ];
+             then
+                echo Your directory concludes
+                ls ${tool}
+             else
+                echo invalid ${tool} not a readable directory
+                exit 
+                fi;;
+            w) test=$OPTARG
+             if [ -d ${test} ];
+             then
+                echo Your directory concludes
+                ls ${test}
+             else
+                echo invalid ${test} not a readable directory
+                exit 
+                fi;;  
+            *) echo "unknown option: $opt";;
+        esac
+    done
+    echo $mode
+    if [ "${mode}" = A ] || [ "${mode}" = B ]
+    then
+    bigboxconf=""
+    print=1
+    elif [ "${mode}" = C ] || [ "${mode}" = D ]
+    then
+    bigboxconf=""
+    print=2
+    fi
+    # output infomation
+    echo !Msg:  ASMtoolsfrom:$(pwd)
+    echo !Msg:  ASMfilefrom:$file
+    echo !Msg:  Mode:$mode Time:$(date)
+    # check the file
+    if [ $mode != 4 ] && [ $mode != 8 ] 
+    then
+        if [ ${file##*.} != ASM ] && [ ${file##*.} != asm ];then
+        echo "the ${file##*.} file may not a assembly source code file"
+        echo ---
+            if ! read -t 10 -p "press Y to use it as an asmfile: " selection || [ ${selection} != Y ]
+            then
+            echo "No \"Y\" catched back to shell"
+            exit
+            fi
+        fi
+        # copy file to test
+        ls ${test} || mkdir ${test} && rm ${test}/T.*
+        echo removed
+        cp "$file" ${test}/T.ASM
+        # echo deleted temp files 
+    fi
+    # do the operation
+        cd ${test}
+        dosbox "${bigboxconf}" -noautoexec\
+        -c "mount c \"$tool\"" -c "mount d \"$test\""\
+        -c "d:"\
+        -c "asm/m ${mode}"
+            echo ASMfilefrom:${file}
+        if [ "$print" = 1 ];then
+            echo "!Msg：[ASM and link] info:"
+            cat T.TXT
+            if [ -r T.OUT ];then
+                echo "!Msg:   [YOUR program] OUTPUT:"
+                cat T.OUT
+                echo 
+            fi
+        elif [ "$print" = 2 ];then
+            tail -n +4 T.TXT
+            if [ -r T.OUT ];then
+                echo "!Msg:   [YOUR program] OUTPUT:"
+                cat T.OUT
+                echo 
+            fi
+        fi
+else
+    echo invalid path $1
+fi
+exit
